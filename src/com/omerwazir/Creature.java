6d0bc92b6fa0b4ca6a9d4741c1ad93c226023432
Omer Wazir <omer@omerwazir.com>
2020-05-08 08:17:17 -0400

    Add  Creature class

---
+++ src/com/omerwazir/Creature.java
@@ -0,0 +1,55 @@
+package com.omerwazir;
+
+import java.awt.*;
+
+public class Creature {
+    private World world;
+    private CreatureAi ai;
+    private final char glyph;
+    private final Color color;
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Creature(World world, char glyph, Color color){
+        this.world = world;
+        this.glyph = glyph;
+        this.color = color;
+    }
+
+    public char getGlyph() {
+        return glyph;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setCreatureAi(CreatureAi ai) {
+        this.ai = ai;
+    }
+
+    public void dig(int wx, int wy) {
+        world.dig(wx, wy);
+    }
+
+    public void moveBy(int mx, int my) {
+        ai.onEnter(x+mx, y+my, world.tile(x+mx, y+my));
+    }
+
+}
