47a4cffe15090b226ed11ef27f882c1075f6f861
Omer Wazir <omer@omerwazir.com>
2020-05-08 08:19:01 -0400

    Update game to add Player and move Player around world.

--- src/com/omerwazir/PlayScreen.java
+++ src/com/omerwazir/PlayScreen.java
@@ -7,15 +7,16 @@ import java.awt.event.KeyEvent;
 public class PlayScreen implements Screen {
 
     private World world;
-    private int centerX;
-    private int centerY;
-    private int screenWidth;
-    private int screenHeight;
+    private final int screenWidth;
+    private final int screenHeight;
+    private final Creature player;
 
     public PlayScreen(){
         screenWidth = 80;
         screenHeight = 21;
         createWorld();
+        CreatureFactory creatureFactory = new CreatureFactory(world);
+        player = creatureFactory.newPlayer();
     }
 
     private void createWorld(){
@@ -23,11 +24,11 @@ public class PlayScreen implements Screen {
     }
 
     public int getScrollX(){
-        return Math.max(0, Math.min(centerX - screenWidth / 2, world.getWidth() - screenWidth));
+        return Math.max(0, Math.min(player.getX() - screenWidth / 2, world.getWidth() - screenWidth));
     }
 
     public int getScrollY(){
-        return Math.max(0, Math.min(centerY - screenHeight / 2, world.getHeight() - screenHeight));
+        return Math.max(0, Math.min(player.getY() - screenHeight / 2, world.getHeight() - screenHeight));
     }
 
     public void displayTiles(AsciiPanel terminal, int left, int top){
@@ -41,34 +42,29 @@ public class PlayScreen implements Screen {
         }
     }
 
-    private void scrollBy(int mx, int my){
-        centerX = Math.max(0, Math.min(centerX + mx, world.getWidth() - 1));
-        centerY = Math.max(0, Math.min(centerY + my, world.getHeight() - 1));
-    }
-
     @Override
     public void displayOutput(AsciiPanel terminal) {
         int left = getScrollX();
         int top = getScrollY();
         displayTiles(terminal, left, top);
-        terminal.write('X', centerX - left, centerY - top);
+        terminal.write(player.getGlyph(), player.getX() - left, player.getY() - top, player.getColor());
     }
 
     @Override
     public Screen respondToUserInput(KeyEvent key) {
         switch (key.getKeyCode()) {
             case KeyEvent.VK_LEFT:
-            case KeyEvent.VK_H: scrollBy(-1, 0); break;
+            case KeyEvent.VK_H: player.moveBy(-1, 0); break;
             case KeyEvent.VK_RIGHT:
-            case KeyEvent.VK_L: scrollBy( 1, 0); break;
+            case KeyEvent.VK_L: player.moveBy( 1, 0); break;
             case KeyEvent.VK_UP:
-            case KeyEvent.VK_K: scrollBy( 0,-1); break;
+            case KeyEvent.VK_K: player.moveBy( 0,-1); break;
             case KeyEvent.VK_DOWN:
-            case KeyEvent.VK_J: scrollBy( 0, 1); break;
-            case KeyEvent.VK_Y: scrollBy(-1,-1); break;
-            case KeyEvent.VK_U: scrollBy( 1,-1); break;
-            case KeyEvent.VK_B: scrollBy(-1, 1); break;
-            case KeyEvent.VK_N: scrollBy( 1, 1); break;
+            case KeyEvent.VK_J: player.moveBy( 0, 1); break;
+            case KeyEvent.VK_Y: player.moveBy(-1,-1); break;
+            case KeyEvent.VK_U: player.moveBy( 1,-1); break;
+            case KeyEvent.VK_B: player.moveBy(-1, 1); break;
+            case KeyEvent.VK_N: player.moveBy( 1, 1); break;
         }
         return this;
     }
