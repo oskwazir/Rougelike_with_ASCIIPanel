e6d56974bb39c16dda021e93ac9e48191be3fdcf
Omer Wazir <omer@omerwazir.com>
2020-05-08 08:17:33 -0400

    Add  CreatureAi class

---
+++ src/com/omerwazir/CreatureAi.java
@@ -0,0 +1,13 @@
+package com.omerwazir;
+
+public class CreatureAi {
+    protected Creature creature;
+
+    public CreatureAi(Creature creature) {
+        this.creature = creature;
+        this.creature.setCreatureAi(this);
+    }
+
+    public void onEnter(int i, int i1, Tile tile) {
+    }
+}
