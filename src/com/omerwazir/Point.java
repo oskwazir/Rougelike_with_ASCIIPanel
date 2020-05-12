c97b61c8144e8ed831d945202071c0048ef81ac7
Omer Wazir <omer@omerwazir.com>
2020-05-12 18:37:05 -0400

    Get nearest 8 neighbours of a point

--- src/com/omerwazir/Point.java
+++ src/com/omerwazir/Point.java
@@ -1,5 +1,9 @@
 package com.omerwazir;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Point {
     public int x;
     public int y;
@@ -9,6 +13,22 @@ public class Point {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public List<Point> nearestEightNeighbours(){
+        List<Point> points = new ArrayList<>();
+
+        for (int ox = -1; ox < 2; ox++){
+            for (int oy = -1; oy < 2; oy++){
+                if (ox == 0 && oy == 0)
+                    continue;
+
+                points.add(new Point(x+ox, y+oy, z));
+            }
+        }
+
+        Collections.shuffle(points);
+        return points;
     }
 
     @Override
