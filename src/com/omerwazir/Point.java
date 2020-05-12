3cde011d70e1ad1293c2ed4ca35bfc6a82e7ba07
Omer Wazir <omer@omerwazir.com>
2020-05-12 18:33:36 -0400

    Add point to help with coordinates

---
+++ src/com/omerwazir/Point.java
@@ -0,0 +1,41 @@
+package com.omerwazir;
+
+public class Point {
+    public int x;
+    public int y;
+    public int z;
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        result = prime * result + z;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Point)) {
+            return false;
+        }
+
+        Point other = (Point) obj;
+
+        return x == other.x && y == other.y && z == other.z;
+    }
+}
